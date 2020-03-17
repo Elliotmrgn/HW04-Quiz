d136d562a76be1af7b50ae8434025468cefdbd1b
Elliot <elliotmrgn@gmail.com>
2020-03-17 02:23:55 -0400

    better but messy

--- script.js
+++ script.js
@@ -18,16 +18,80 @@ const myQuestions = [
         correctAnswer: "2"
     },
     {
-        question: "Which tool can you use to ensure code quality?",
+        question: "Inside which HTML element do we put the JavaScript?",
         answers: {
-            0: "Angular",
-            1: "jQuery",
-            2: "RequireJS",
-            3: "ESLint"
+            0: "<js>",
+            1: "<script>",
+            2: "<javascript>",
+            3: "<scripting>"
         },
-        correctAnswer: "3"
+        correctAnswer: "1"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'script.js'?",
+        answers: {
+            0: "<script href='script.js'>",
+            1: "<script src='script.js'>",
+            2: "<script name='script.js'>"
+        },
+        correctAnswer: "1"
+    },
+    {
+        question: "The external JavaScript file must contain the < script > tag.",
+        answers: {
+            0: "True",
+            1: "False"
+        },
+        correctAnswer: "1"
+    },
+    {
+        question: "What will the following code return: Boolean(10 > 9)",
+        answers: {
+            0: "true",
+            1: "NaN",
+            2: "false"
+        },
+        correctAnswer: "0"
+    },
+    {
+        question: "How can you detect the client's browser name?",
+        answers: {
+            0: "client.navName",
+            1: "browser.name",
+            2: "navigator.appName"
+        },
+        correctAnswer: "2"
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        answers: {
+            0: "onclick",
+            1: "onmouseclick",
+            2: "onmouseover",
+            3: "onchange"
+        },
+        correctAnswer: "0"
+    },
+    {
+        question: "How do you find the number with the highest value of x and y?",
+        answers: {
+            0: "ceil(x, y)",
+            1: "Math.max(x, y)",
+            2: "Math.ceil(x, y)",
+            3: "top(x, y)"
+        },
+        correctAnswer: "1"
+    },
+    {
+        question: "JavaScript is case-sensitive?",
+        answers: {
+            0: "True",
+            1: "False"
+        },
+        correctAnswer: "0"
     }
 ];
+
 var headerText = $("#card-header-text");
 var bodyText = $("#card-body");
 var timerText = $("#timer");
@@ -39,6 +103,30 @@ var questionNumber = 0;
 var time = 75;
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 var interval;
+
+
+
+//build the current quiz question
+function quiz() {
+    //if there's still a question to build
+    if (questionNumber < myQuestions.length) {
+        bodyText.html(myQuestions[questionNumber].question);
+        buttons.empty();
+        for (var i = 0; i < Object.keys(myQuestions[questionNumber].answers).length; i++) {
+            var answerButton = $("<button>");
+            answerButton.addClass("quiz-button");
+            answerButton.attr("answer-choice", i);
+            answerButton.text(myQuestions[questionNumber].answers[i]);
+            buttons.append(answerButton);
+        }
+
+        $(".quiz-button").click(answerCheck)
+    }
+    else {
+        endGame();
+    }
+
+}
 
 function answerCheck() {
     if ($(this).attr("answer-choice") === myQuestions[questionNumber].correctAnswer) {
@@ -55,96 +143,90 @@ function answerCheck() {
     }
 }
 
-function quiz() {
-    if (questionNumber < myQuestions.length) {
-        bodyText.html(myQuestions[questionNumber].question);
-        buttons.empty();
-        for (var i = 0; i < Object.keys(myQuestions[questionNumber].answers).length; i++) {
-            var answerButton = $("<button>");
-            answerButton.addClass("quiz-button");
-            answerButton.attr("answer-choice", i);
-            answerButton.text(myQuestions[questionNumber].answers[i]);
-            buttons.append(answerButton);
-        }
-
-        $(".quiz-button").click(answerCheck)
-    }
-    else{
-        endGame();
-    }
-}
-
-function setScore(){
-    const score = {
+function setScore() {
+    var score = {
         name: highScoreName,
         score: time
     };
     highScores.push(score);
 
-    highScores.sort( (a,b) => b.score - a.score)
+    highScores.sort((a, b) => b.score - a.score)
 
     highScores.splice(10);
     localStorage.setItem("highScores", JSON.stringify(highScores));
 
 }
 
-function highScoreScreen(){
+function highScoreScreen() {
     clearInterval(interval);
     bodyText.empty();
     buttons.empty();
-    var storedHighScores = JSON.parse(localStorage.getItem("highScores"));
-    var ul = $("<ul>");
-    for (var i =0; i<storedHighScores.length; i++){
-        var li = $("<li>");
-        li.html(storedHighScores[i].name + ": " + storedHighScores[i].score);
-        ul.append(li);
+    if (localStorage.getItem("highScores") !== null) {
+        var storedHighScores = JSON.parse(localStorage.getItem("highScores"));
+        var ol = $("<ol style='list-style-position:inside; '>");
+        for (var i = 0; i < storedHighScores.length; i++) {
+            var li = $("<li>");
+            li.html(storedHighScores[i].name + ": " + storedHighScores[i].score);
+            ol.append(li);
+        }
+        bodyText.append(ol);
     }
-    bodyText.append(ul);
-    
+    var clearScores = $("<button style='background-color:red';>");
+    clearScores.text("Clear HighScores");
+    bodyText.append(clearScores);
+    clearScores.click(function () {
+        if (confirm("Are you sure?\nThis cannot be undone")) {
+            localStorage.removeItem("highScores");
+            highScores = [];
+            highScoreScreen();
+        }
+    });
+
+
 }
 
-function endGame(){
+function endGame() {
     clearInterval(interval);
     buttons.empty();
     bodyText.html("Game Over!<br />Final Score: " + time + "<br />Enter Intials to save high score<br />");
     bodyText.append("<input id='highscore-input' type='text' placeholder='Initials' maxlength=3 size=3>")
-    bodyText.append("<input id='highscore-submit' type='submit'>");
-    bodyText.on("click", "#highscore-submit", function(){
-        highScoreName = $("#highscore-input").val();
-        setScore()
-        highScoreScreen();
+    bodyText.append("<input id='highscore-submit' type='submit' style='margin-left:5px';>");
+    bodyText.on("click", "#highscore-submit", function () {
+        if ($("#highscore-input").val()) {
+            highScoreName = $("#highscore-input").val();
+            setScore()
+            highScoreScreen();
+        }
     });
 }
 
 function timer() {
-
+    clearInterval(interval);
     interval = setInterval(function () {
         time--;
         timerText.html("Time: " + time);
         if (time <= 0) {
-            clearInterval(interval);
+            endGame();
         }
     }, 1000)
 }
 
-/* highScoreLink.click(function(){
-    clearInterval(interval);
-
-}) */
-
 function init() {
+    clearInterval(interval);
+    questionNumber = 0;
+    time = 75;
     highScoreLink.click(highScoreScreen);
     timerText.html("Time: " + time);
     headerText.html("Coding Quiz");
-    bodyText.html("10 questions<br />Fastest time wins<br />-10 seconds per wrong answer<br />Press start to begin");
+    bodyText.html("10 questions<br />-10 seconds per wrong answer<br />Fastest time wins");
     buttons.html("<button id='start-btn'>Begin Quiz</button>");
+    $("header").on("click", "#restart", init);
     buttons.on("click", "#start-btn", function () {
         timer();
         quiz();
     });
-    
-}
 
+}
 
 init();
 
